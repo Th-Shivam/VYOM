9945e4c645863edd827d5cd4f1686b4482660892
Th-Shivam <dreamyshivam01@gmail.com>
2025-06-06 12:01:19 +0530

    VYOM 1.0

---
+++ Backend/Automation.py
@@ -0,0 +1,219 @@
+# Import required libraries
+from AppOpener import close, open as appopen  # Import functions to open and close apps.
+from webbrowser import open as webopen  # Import web browser functionality.
+from pywhatkit import search, playonyt  # Import functions for search and YouTube playback.
+from dotenv import dotenv_values  # Import dotenv to manage environment variables.
+from bs4 import BeautifulSoup  # Import BeautifulSoup for parsing HTML content.
+from rich import print  # Import rich for styled console output.
+from groq import Groq  # Import Groq for AI Chat functionalities.
+import webbrowser  # Import webbrowser for opening URLs.
+import subprocess  # Import subprocess for interacting with the system.
+import requests  # Import requests for making HTTP requests.
+import keyboard  # Import keyboard for keyboard-related actions.
+import asyncio  # Import asyncio for asynchronous programming.
+import os  # Import os for operating system functionalities.
+
+# Load environment variables from the .env file.
+env_vars = dotenv_values(".env")
+GroqAPIKey = env_vars.get("GroqAPIKey")  # Retrieve the Groq API key.
+
+# Define CSS classes for parsing specific elements in HTML content.
+classes = ["zCubwf", "hgKElc", "LTKOO sY7ric", "Z0LcW", "gsrt vk_bk FzvWSb YwPhnf", "pclqee", "tw-Data-text tw-text-small tw-ta",
+           "IZ6rdc", "O5uR6d LTKOO", "vlzY6d", "webanswers-webanswers_table__webanswers-table", "dDoNo ikb4Bb gsrt", "sXLaOe",
+           "LWkfKe", "VQF4g", "qv3Wpe", "kno-rdesc", "SPZz6b"]
+
+# Define a user-agent for making web requests.
+useragent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36'
+
+# Initialize the Groq client with the API key.
+client = Groq(api_key=GroqAPIKey)
+
+# Predefined professional responses for user interactions.
+professional_responses = [
+    "Your satisfaction is my top priority; feel free to reach out if there's anything else I can help you with.",
+    "I'm at your service for any additional questions or support you may need—don't hesitate to ask.",
+]
+
+# List to store chatbot messages.
+messages = []
+
+# System message to provide context to the chatbot.
+SystemChatBot = [{"role": "system", "content": f"Hello, I am {os.environ['Username']}. You're a content writer. You have to write content like letters, codes , applications , emails, articles, and other professional documents as requested."}]
+
+# Function to perform a Google search.
+def GoogleSearch(Topic):
+    search(Topic)  # Use pywhatkit's search function to perform a Google search.
+    return True  # Indicate success.
+
+# Function to generate content using AI and save it to a file.
+def Content(Topic):
+    
+    # Nested function to open a file in Notepad.
+    def OpenNotepad(File):
+        default_text_editor = 'notepad.exe'  # Default text editor.
+        subprocess.Popen([default_text_editor, File])  # Open the file in Notepad.
+    
+    # Nested function to generate content using the AI chatbot.
+    def ContentWriterAI(prompt):
+        messages.append({"role": "user", "content": f"{prompt}"})  # Add the user's prompt to messages.
+        
+        completion = client.chat.completions.create(
+            model="llama3-70b-8192",  # Specify the AI model.
+            messages=SystemChatBot + messages,  # Include system instructions and chat history.
+            max_tokens=2048,  # Limit the maximum tokens in the response.
+            temperature=0.7,  # Adjust response randomness.
+            top_p=1,  # Use nucleus sampling for response diversity.
+            stream=True,  # Enable streaming response.
+            stop=None  # Allow the model to determine stopping conditions.
+        )
+        
+        Answer = ""  # Initialize an empty string for the response.
+        
+       # Process streamed response chunks.
+        for chunk in completion:
+            if chunk.choices[0].delta.content:  # Check for content in the current chunk.
+                Answer += chunk.choices[0].delta.content  # Append the content to the answer.
+
+        Answer = Answer.replace("</s>", "")  # Remove unwanted tokens from the response.
+        messages.append({"role": "assistant", "content": Answer})  # Add the AI's response to messages.
+        return Answer
+
+    Topic: str = Topic.replace("Content ", "")  # Remove 'Content ' from the topic.
+    ContentByAI = ContentWriterAI(Topic)  # Generate content using AI.
+
+    # Save the generated content to a text file.
+    with open(rf"Data\{Topic.lower().replace(' ', '')}.txt", "w", encoding="utf-8") as file:
+        file.write(ContentByAI)  # Write the content to the file.
+        file.close()
+
+    OpenNotepad(rf"Data\{Topic.lower().replace(' ', '')}.txt")
+    return True  # Indicate success.
+
+def YoutubeSearch(Topic):
+    Url4Search = f"https://www.youtube.com/results?search_query={Topic}"
+    webbrowser.open(Url4Search)  # Open the YouTube search results in the web browser.
+    return True  # Indicate success.
+
+def PlayYoutube(query):
+    playonyt(query)  # Use pywhatkit to play the specified topic on YouTube.
+    return True  # Indicate success.
+
+def OpenApp(app , sess=requests.session()):
+
+    try:
+        appopen(app, match_closest=True, output=True)
+    except:
+        def extract_links(html):
+            if html is None:
+                return []
+            soup = BeautifulSoup(html, 'html.parser')
+            links = soup.find_all('a', {'jsname':'UWckNb'})
+            return [link.get('href') for link in links]
+
+
+        def search_google(query):
+            url = f"https://www.google.com/search?q={query}"
+            headers = {"User_Agent" : useragent}
+            response =  sess.get(url, headers=headers)
+
+            if response.status_code == 200:
+                return response.text
+            else:
+                print("Failed to retrive search results.")
+            return None
+        
+        html = search_google(app)
+
+        if html:
+            link = extract_links(html)[0]
+            webopen(link)
+
+        return True
+
+def CloseApp(app):
+
+    if "chrome" in app:
+        pass
+    else:
+        try:
+            close(app, match_closest=True , output=True , throw_error = True)
+            return True
+        except:
+            return False
+        
+def System(command):
+    def mute():
+        keyboard.press_and_release("volume mute")
+
+    def unmute():
+        keyboard.press_and_release("volume mute")
+
+    def volume_up():
+        keyboard.press_and_release("volume up")
+
+    def volume_down():
+        keyboard.press_and_release("volume down")
+
+    if command == "mute":
+        mute()
+    elif command == "unmute":
+        unmute()
+    elif command == "volume up":
+        volume_up()
+
+    elif command == "volume down":
+        volume_down()
+
+    return True
+
+
+async def TranslateAndExecute(commands: list[str]):
+    funcs = []
+
+    for command in commands:
+        if command.startswith("open "):
+            if "open it" in command:
+                pass
+            if "open file" in command:
+                pass
+            else:
+                fun = asyncio.to_thread(OpenApp, command.removeprefix("open "))
+                funcs.append(fun)
+
+        elif command.startswith("general "):
+            pass
+        elif command.startswith("realtime "):
+            pass
+        elif command.startswith("close "):
+            fun = asyncio.to_thread(CloseApp, command.removeprefix("close "))
+            funcs.append(fun)
+        elif command.startswith("play "):
+            fun = asyncio.to_thread(PlayYoutube, command.removeprefix("play "))
+            funcs.append(fun)
+        elif command.startswith("content "):
+            fun = asyncio.to_thread(Content, command.removeprefix("content "))
+            funcs.append(fun)
+        elif command.startswith("google search "):
+            fun = asyncio.to_thread(GoogleSearch, command.removeprefix("google search "))
+            funcs.append(fun)
+        elif command.startswith("youtube search "):
+            fun = asyncio.to_thread(YoutubeSearch, command.removeprefix("youtube search "))
+            funcs.append(fun)
+        elif command.startswith("system "):
+            fun = asyncio.to_thread(System, command.removeprefix("system "))
+            funcs.append(fun)
+        else:
+            print(f"No Functions Found For {command}")
+    results =   await asyncio.gather(*funcs)
+
+    for result in results:
+        if isinstance(result, str):
+            yield result
+        else:
+            yield result
+
+async def Automation(commands: list[str]):
+    async for result in TranslateAndExecute(commands):
+        pass
+
+    return True
---
+++ Backend/ChatBot.py
@@ -0,0 +1,118 @@
+from groq import Groq 
+from json import load , dump
+import datetime
+from dotenv import dotenv_values
+
+#load env vars fromm the .env file
+
+env_vars = dotenv_values(".env")
+
+#retrieve specific env vars for username , assistant name and API Key
+
+Username = env_vars.get("Username")
+AssistantName = env_vars.get("AssistantName")
+GroqAPIKey = env_vars.get("GroqAPIKey")
+
+#initialize the Groq client with the API key
+client =  Groq(api_key=GroqAPIKey)
+
+#initialize an empty list to store messages
+messages = []
+
+#defiine a system msg that provides conext to the model about its role and behavior
+System = f"""Hello, I am {Username}, You are a very accurate and advanced AI chatbot named {AssistantName} which also has real-time up-to-date information from the internet.
+*** Do not tell time until I ask, do not talk too much, just answer the question.***
+*** Reply in only English, even if the question is in Hindi, reply in English.***
+*** Do not provide notes in the output, just answer the question and never mention your training data. ***
+"""
+
+
+#a list of system inst for the chatbot 
+
+SystemChatBot = [
+    {"role":"system", "content": System },
+]
+
+# Attempt to load the chat history from a JSON file
+
+try:
+    with open(r"Data\ChatLog.json" , "r") as f:
+        messages = load(f)
+except FileNotFoundError:
+
+    # If the file does not exist, initialize messages with the system chat bot
+    with open (r"Data\ChatLog.json", "w") as f:
+        dump([],f)
+
+# Function to get real time date and time info 
+
+def RealTimeInformation():
+    current_date_time = datetime.datetime.now()
+    day = current_date_time.strftime("%A")
+    date = current_date_time.strftime("%d")
+    month = current_date_time.strftime("%B")
+    year = current_date_time.strftime("%Y") 
+    hour = current_date_time.strftime("%H")
+    minute = current_date_time.strftime("%M")
+    second = current_date_time.strftime("%S")
+
+    #format the info into a string 
+
+    data = f"Please use this real-time information if needed , \n"
+    data += f"Day : {day}\nDate: {date}\nMonth: {month}\nYear: {year}\n"
+    data += f"Time: {hour} hours : {minute} minutes : {second} seconds\n"
+    return data
+
+#func to modify the chatbot's response for better formatting . 
+
+def AnswerModifier(Answer):
+    lines = Answer.split('\n')
+    non_empty_lines = [line for line in lines if line.strip()]
+    modified_answer = '\n'.join(non_empty_lines)
+    return modified_answer
+
+def ChatBot(Query):
+    #this function sends the user query to the Groq model and returns the response
+    try:
+        with open (r"Data\ChatLog.json", "r") as f:
+            messages = load(f)
+
+    # append the userss query to the messages list
+        messages.append({"role": "user", "content": f"{Query}"})
+
+    # makes a request to the Groq model with the messages and system context
+        completion = client.chat.completions.create(
+            model="llama3-70b-8192" , 
+            messages = SystemChatBot + [{"role": "system", "content": RealTimeInformation()}] + messages,
+            max_tokens=1024 , 
+            temperature=0.7,
+            top_p=1 ,
+            stream=True,
+            stop=None 
+        )
+
+        Answer = ""
+
+        for chunk in completion:
+            if chunk.choices[0].delta.content:
+                Answer += chunk.choices[0].delta.content
+        
+        Answer = Answer.replace("</s>", "")
+
+        messages.append({"role": "assistant", "content": Answer})
+
+        with open(r"Data\ChatLog.json", "w") as f:
+            dump(messages, f, indent=4)
+        return AnswerModifier(Answer = Answer)
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        with open(r"Data\ChatLog.json", "w") as f:
+            dump([], f, indent=4)   
+        return ChatBot(Query)
+
+if __name__ == "__main__":
+    while True:
+        user_input = input("Enter Your Question : ")
+        print(ChatBot(user_input))
+ 
